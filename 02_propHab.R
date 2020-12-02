e58a1d107eb08838d00b42b809e68b08db5d21ac
Willian <willian.vieira@usherbrooke.ca>
2020-12-02 14:51:51 -0500

    📈 get prob for all hexagons and plot propNA

--- 02_propHab.R
+++ 02_propHab.R
@@ -124,27 +124,26 @@ for (id in unique(districts$ECOREGION)) {
 
     # Extract habitat values within each hexagons -- FOR CA
     print(glue('Start to extract habitat values for CA'))
-    hab_ca <- foreach(i = seq_len(nrow(hexa_ecoregion)), .packages = "raster", .combine = c) %dopar% {    
-        count_ca <- as.data.frame(table(extract(land_ca, hexa_ecoregion[i, ])))
+    hab_ca <- foreach(i = seq_len(nrow(hexa_ecoregion)), .packages = "raster") %dopar% {    
+        values_ca <- extract(land_ca, hexa_ecoregion[i, ])[[1]]
+        count_ca <- as.data.frame(table(values_ca))
         if(nrow(count_ca) > 0)
         {
-            if(sum(count_ca$Freq) > 1000) # ensure that hexagon has at least 100 pixels for secondary sampling
-            {
-                prev_count_hab <- merge(count_ca, prev_ca_ecoregion, by.x="Var1", by.y="code" ,all.x=TRUE)
-                sum(prev_count_hab$Freq * prev_count_hab$incl_prob)
-            } else {
-                NA
-            }
+            # save proportion of empty pixels in the hexagonq
+            propNA <- sum(is.na(values_ca))/length(values_ca)
+            # probability of inclusion
+            prev_count_hab <- merge(count_ca, prev_ca_ecoregion, by.x = "values_ca", by.y = "code" , all.x = TRUE)
+            # return
+            c(propNA, sum(prev_count_hab$Freq * prev_count_hab$incl_prob))
         } else {
-            NA
+            c(NA, NA)
         }
     }
 
-    # Assigning new columns with hab prob
-    hexa_ecoregion$hab_ca <- hab_ca
-    #hexa_ecoregion$hab_qc <- hab_qc
+    # Assigning new columns with hab prob and proportion of NA in the hexagon attributes table
+    hexa_ecoregion$hab_ca <- unlist(lapply(hab_ca, `[[`, 2))
+    hexa_ecoregion$propNA <- unlist(lapply(hab_ca, `[[`, 1))
 
-    # Add new phab columns in attributes table
     print(glue('Saving'))
 
     # Writing results in corresponding folder
@@ -155,3 +154,28 @@ for (id in unique(districts$ECOREGION)) {
 }
 
 stopImplicitCluster()
+
+
+
+
+###############################################
+# Plot percentage of NA for each hexagon
+###############################################
+
+pdf('proportionNA_ecoregion.pdf', width = 12, height = 6)
+for(id in unique(districts$ECOREGION))
+{
+    # Select ecoregion
+    ecoregion <- districts[districts$ECOREGION == id, ]
+
+    # load ecoregion hexagons
+    hexa_ecoregion <- sf::st_read(paste0('output/', tolower(gsub(' ', '_', unique(ecoregion$REGION_NAM))), '_', id, '/hexa_phab.shp'), quiet = TRUE)
+
+    # plot
+    par(mfrow = c(1, 2), mar = c(2.5, 2.5, 2.5, 0.5), mgp = c(1.4, 0.2, 0), tck = -.008)
+    hist(hexa_ecoregion$hab_ca, main = '', ylab = 'Frequency (# of hexagons)', xlab = 'Habitat probability of hexagons', breaks = 30)
+    hist(hexa_ecoregion$propNA * 100, main = '', ylab = 'Frequency (# of hexagons)', xlab = 'Proportion of NA pixels within each hexagon (%)', breaks = 30)
+    mtext(paste('Ecoregion', id, '-', unique(ecoregion$REGION_NAM)), 3, line = -2, outer = TRUE)
+
+}
+dev.off()
