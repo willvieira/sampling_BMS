2b371e475684450db06b974efbb7297d04211b21
Willian <willian.vieira@usherbrooke.ca>
2021-01-25 08:48:33 -0500

    ✨ cost surface using Ontario methods

--- 03_costSurface.R
+++ 03_costSurface.R
@@ -16,49 +16,80 @@ load("data/spatialVectors.rda")
 land_ca <- raster::raster("data/landcover_ca_30m.tif")
 
 
-# crop lines (roads, trails, aeroports) for the study area
-roads_area <- sf::st_crop(roads, area)
-trails_area <- sf::st_crop(trails, area)
-aeroports_area <- sf::st_crop(aeroports, area)
 
-# View spatial cost objects
-mapview(roads_area) + mapview(trails_area) + mapview(aeroports_area)
+#####################################################################
+# Define cost parameters
+#####################################################################
 
+# number of ARUs to be deployed for each hexagon
+params <- list(
+  nb_arus = 5.0,
+  # Truck cost
+  truck_buffer = 1000, # meters
+  truck_cost_per_day = 600.0,
+  truck_n_crews	= 2.0,
+  truck_arus_per_crew_per_day	= 5.0,
+  # ATV cost
+  atv_buffer = 1000,
+  atv_cost_per_day = 1200.0,
+  atv_n_crews	= 2.0,
+  atv_arus_per_crew_per_day	= 3.0,
+  # Helicopter
+  helicopter_cost_per_hour = 1250.0,
+  helicopter_max_km_from_base	= 150.0,
+  helicopter_base_setup_cost_per_km =	9.0,
+  helicopter_l_per_hour = 160.0,
+  helicopter_crew_size = 4.0,
+  helicopter_aru_per_person_per_day	= 5.0,
+  helicopter_relocation_speed = 180.0,
+  helicopter_airport_cost_per_l	= 1.3,
+  helicopter_base_cost_per_l	= 5.0,
+  helicopter_2nd_base_cost_per_l = 10.0,
+  helicopter_hours_flying_within_sa_per_day	= 5.0
+)
 
 
 #####################################################################
-# Roads
+# Roads (truck)
 #####################################################################
 
-# 2km buffer around all roads
-road_buff <- sf::st_buffer(roads_area, dist = 2000)
+# Buffer around all roads
+road_buff <- sf::st_buffer(roads, dist = params$truck_buffer)
+
+# Standardize all polygons type to polygon (fasterize accepts only ONE type of geometry)
+road_buff <- sf::st_cast(road_buff, 'POLYGON')
 
 # rasterize road buffer
 road_buff_r <- fasterize::fasterize(road_buff, land_ca)
 
-# Add a fixed cost of 100 $ for the 2km buffer
-road_cost <- raster::reclassify(road_buff_r, cbind(1, 100))
+# Add a fixed cost depending on parameters for the buffer
+truckFixedCost <- params$truck_cost_per_day * params$nb_arus / params$truck_arus_per_crew_per_day
+road_cost <- raster::reclassify(road_buff_r, cbind(1, truckFixedCost))
 
 # clean memory
-rm(road_buff, road_buff_r, roads, roads_area)
+rm(road_buff, road_buff_r, roads)
 
 
 
 #####################################################################
-# Trails
+# ATV (trails)
 #####################################################################
 
-# 300 m buffer around all roads
-trail_buff <- sf::st_buffer(trails_area, dist = 300)
+# Buffer around all ATV trails
+atv_buff <- sf::st_buffer(trails, dist = params$atv_buffer)
+
+# Standardize all polygons type to polygon (fasterize accepts only ONE type of geometry)
+atv_buff <- sf::st_cast(atv_buff, 'POLYGON')
 
 # rasterize road buffer
-trail_buff_r <- fasterize::fasterize(trail_buff, land_ca)
+atv_buff_r <- fasterize::fasterize(atv_buff, land_ca)
 
-# Add a fixed cost of 200 $ for the 300 m buffer
-trail_cost <- raster::reclassify(trail_buff_r, cbind(1, 200))
+# Add a fixed cost for the buffer
+atvFixedCost <- params$atv_cost_per_day * params$nb_arus / params$atv_arus_per_crew_per_day
+atv_cost <- raster::reclassify(atv_buff_r, cbind(1, atvFixedCost))
 
 # clean memory
-rm(trail_buff, trail_buff_r, trails, trails_area)
+rm(atv_buff, atv_buff_r, trails)
 
 
 
@@ -66,47 +97,101 @@ rm(trail_buff, trail_buff_r, trails, trails_area)
 # Helicopter access
 #####################################################################
 
-heli_buff <- trail_cost
-values(heli_buff) <- NA
+# Get centroid of hexagons
+# Helicoter cost is calculated once for the whole hexagon based on its centroid (not pixel based)
+hexa_hel <- hexa
+hexa_hel$geometry <- hexa %>% sf::st_centroid() %>% sf::st_geometry()
 
-# Two classes of distance (45, 150)
-# Everything farther to 250 km will have probability of zero (not yet)
-heli_dist <- raster::distanceFromPoints(heli_buff, as(aeroports_area[['Shape']], 'Spatial'))
+# Fix projection and calculate matrix of distance between centroid and aeroports
+aeroports <- sf::st_transform(aeroports, sf::st_crs(hexa_hel))
+mat_dist <- sf::st_distance(x = hexa_hel, y = aeroports)
+hexa_hel$aeroportDist <- apply(mat_dist, 1, min)
 
-# Add fixed cost depending on distance classes
-heli_cost <- raster::calc(heli_dist, function(x) {
-  ifelse(x <= 45000, 1000, ifelse(x < 150000, ((x / 1000) * 2 * 16.23) - 460.70, ((x / 1000) * 2 * 16.23) + 239.04))
-})
+# Add cost depending on distance (meters)
+heliCost <- function(distance, pars)
+{
+  # Transform distance to km
+  distance <- distance/1000
+
+  # Cost per litre of fuel (depending on distance)
+  Cl <- ifelse(distance < pars$helicopter_max_km_from_base,
+                pars$helicopter_airport_cost_per_l,
+                ifelse(distance < (2 * pars$helicopter_max_km_from_base),
+                  pars$helicopter_base_cost_per_l,
+                  pars$helicopter_2nd_base_cost_per_l))
+
+  # Cost per hour of helicopter (rent + fuel)/h
+  Ch <- pars$helicopter_l_per_hour * Cl + pars$helicopter_cost_per_hour
+
+  # Cost per Km of helicopter (rent + fuel)
+  Cd <- Ch/pars$helicopter_relocation_speed
+
+  # Hours per ARUs deployed (depending on h/day of helicopter, number of crew, and AURs per crew)
+  H_arus <- pars$helicopter_hours_flying_within_sa_per_day/(pars$helicopter_crew_size * pars$helicopter_aru_per_person_per_day)
+
+  # If basecamp needed, calculate:
+  # - distance from (A)report to (B)asecamp
+  # - distance from (B)asecamp to (H)exagon
+  # - Cb - cost of flying from aerport to basecamp
+  if(distance >= pars$helicopter_max_km_from_base)
+  {
+    distanceBH <- distance - pars$helicopter_max_km_from_base
+    distanceAB <- distance - distanceBH
+    
+    # Cost of flying from Aeroport to basecamp
+    Cb <- (distanceAB * pars$helicopter_base_setup_cost_per_km) + 
+          (2 * distanceAB/pars$helicopter_relocation_speed * pars$helicopter_l_per_hour * Cl)
+  }else{
+    Cb <- 0
+  }
+
+  # Cost within hexagon (number of ARUs * Hours per ARU * cost per hour)
+  Csa <- pars$nb_arus * H_arus * Ch
+
+  # Cost of flying to the hexagon (either from Aerport OR basecamp)
+  if(distance >= pars$helicopter_max_km_from_base)
+  {
+    Cf <- 2 * distanceBH * Cd
+  }else{
+    Cf <- 2 * distance * Cd
+  }
+
+  # final cost to get to a hexagon
+  Cost_heli <- Csa + Cf + Cb
+}
+
+hexa_hel$heliCost <- sapply(hexa_hel$aeroportDist, heliCost, params)
 
 # clean memory
-rm(heli_dist, heli_buff)
+rm(mat_dist)
 
 
 
 #####################################################################
-# Stack the three layers cost (roads, trails, and helicopters)
+# Stack the two layers cost (roads + trails)
 #####################################################################
 
 # stack all three layers
-cost_stack <- raster::stack(road_cost, trail_cost, heli_cost)
+cost_stack <- raster::stack(road_cost, atv_cost)
 
 # get minimum cost for each cell
 cost_min <- raster::calc(cost_stack, min)
 
 # save
 raster::writeRaster(cost_min, filename = 'data/min_cost.tif', format = 'GTiff')
-rm(cost_stack)
+saveRDS(hexa_hel, 'data/hexa_hel_cost.RDS')
 
 
 
 
 
 #####################################################################
-# Extract mean cost for each exagon
+# Extract mean cost for each hexagon
 #####################################################################
 
 # load min cost raster calculated above
 cost_min <- raster::raster('data/min_cost.tif')
+hexa_hel <- readRDS('data/hexa_hel_cost.RDS')
 
 
 for (id in unique(districts$ECOREGION)) {
@@ -117,19 +202,37 @@ for (id in unique(districts$ECOREGION)) {
     ecoregion <- districts[districts$ECOREGION == id, ]
 
     # Load hexagons of ecoregion (generated from `script 02_probHab.R`)
-    hexa_ecoregion <- sf::st_read(paste0('output/', tolower(gsub(' ', '_', unique(ecoregion$REGION_NAM))), '_', id, '/hexa_phab.shp'))
+    hexa_ecoregion <- sf::st_read(paste0('output/', tolower(gsub(' ', '_', unique(ecoregion$REGION_NAM))), '_', id, '/hexa_phab.shp'), quiet = TRUE)
 
+    # Crop cost raster to ecoregion
+    print('Start to calculate the cost-based inclusion probabilities')
+    costMin_ecoregion <- mask(crop(cost_min, as(hexa_ecoregion, "Spatial")), as(hexa_ecoregion, "Spatial"))
+    
     # Extract habitat values within each hexagons -- FOR CA
-    print('Start to extract habitat values for CA')
-    costMin <- foreach(i = seq_len(nrow(hexa_ecoregion)), .packages = "raster", .combine = c) %dopar% {    
-        raster::extract(cost_min, hexa_ecoregion[i, ], fun = mean)
+    print('Start to extract habitat values for each hexagon')
+    costSum <- foreach(i = seq_len(nrow(hexa_ecoregion)), .packages = "raster") %dopar% {    
+      hexValues <- raster::extract(costMin_ecoregion, hexa_ecoregion[i, ])
+      costTab <- table(hexValues[[1]])
+      propTransport <- setNames(rep(0, 2), c(as.character(params$truck_cost_per_day),
+                                           as.character(params$atv_cost_per_day)))
+      if(nrow(costTab) > 0)
+        propTransport[names(costTab)] <- costTab/length(hexValues[[1]])
+      
+      # add helicopter (vector name is the helicopter cost for the hexagon to facilitate calculation)
+      heli_cost_hex_i <- as.character(subset(hexa_hel, ET_ID == hexa_ecoregion[i, ]$ET_ID)$heliCost)
+      propTransport[heli_cost_hex_i] <- 1 - sum(propTransport)
+
+      # calculate weighted cost
+      sum(as.numeric(names(propTransport)) * propTransport)
     }
 
     # Assigning new columns with hab prob
-    hexa_ecoregion$cost_min <- costMin
-    #hexa_ecoregion$hab_qc <- hab_qc
+    hexa_ecoregion$costSum <- unlist(costSum)
 
-    # Add new phab columns in attributes table
+    # Calculate the root squared of inversed cost to get a probabilistic value
+    inv_sqrt <- 1 / (sqrt(unlist(costSum)))
+    hexa_ecoregion$costProb <- inv_sqrt / sum(inv_sqrt)
+
     print('Saving')
 
     # Writing results in corresponding folder
@@ -137,6 +240,25 @@ for (id in unique(districts$ECOREGION)) {
 }
 
 stopImplicitCluster()
+
+
+
+
+#####################################################################
+# Print hexagon cost
+#####################################################################
+
+eco_missingHexa <- c(7, 28, 47, 48, 46, 31, 30, 131)
+
+# load shapefiles into a list
+cost_ls <- list()
+for (id in setdiff(unique(districts$ECOREGION), eco_missingHexa))
+  cost_ls[[as.character(id)]] <- sf::st_read(paste0('output/', tolower(gsub(' ', '_', unique(subset(districts, ECOREGION == id)$REGION_NAM))), '_', id, '/hexa_cost.shp'), quiet = TRUE)
+
+# rbind all shapefiles into one sf oject
+cost_bind <- do.call(rbind, cost_ls)
+
+sf::write_sf(cost_bind, 'cost_Ontario.shp')
 
 
 
